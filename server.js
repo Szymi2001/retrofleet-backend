927e3e4d50428a11319f10e0c7237eca53cacc73
Szymi2001 <szymonkowalczyk2707@gmail.com>
2024-10-13 20:54:47 +0200

    changed cors again

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ const options = {
     cert: fs.readFileSync(certPath),
   };
 
-app.use(cors({ origin: '*'}));
+app.use(cors({ 
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 app.use(express.json())
 
