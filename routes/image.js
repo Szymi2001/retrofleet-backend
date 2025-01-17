ef53d592617ddb5d586e622745fd95b397c99f95
Szymi2001 <szymonkowalczyk2707@gmail.com>
2025-01-17 14:10:19 +0100

    repair carId return

--- routes/image.js
+++ routes/image.js
@@ -63,10 +63,13 @@ router.get('/download/:userId', async (req, res) => {
         });
 
         // Mapowanie wyników na listę URL-i obrazów
-        const imageDetails = result.resources.map(resource => ({
-            carId: resource.public_id,
-            url: resource.secure_url
-        }));
+        const imageDetails = result.resources.map(resource => {
+            const carId = resource.public_id.split('/').pop();
+            return {
+                carId: carId,
+                url: resource.secure_url
+            };  
+        });
 
         res.status(200).json({
             images: imageDetails
